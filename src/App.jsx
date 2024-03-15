23cc366e314f7d4cdf427a031ead3383e8b64979
AHNAYEEM40 <ahnayeem40@gmail.com>
2024-03-15 15:37:14 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Component/header/Header'
+import Blogs from './Component/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +9,8 @@ function App() {
   return (
     <>
 
-      <h1 className='text-6xl bg-red-600'>Knowledge Csfe  </h1>
-      
-
+<Header></Header>
+<Blogs></Blogs>
     </>
   )
 }
